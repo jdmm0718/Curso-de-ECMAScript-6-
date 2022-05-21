97fd3d0dcbfdc483446e87232437d2972df614ab
jdmm0718 <juandavidmm0718@gmail.com>
2022-05-20 23:16:03 -0500

    clase 3

---
+++ src/es6/index.js
@@ -0,0 +1,186 @@
+const obj = {
+    name: 'oscar',
+    age: 32,
+    country: 'MX'
+  };
+  
+  let { country, ...all } = obj;
+  console.log(all);
+  
+  const obje = {
+    name: 'Oscar',
+    age: 32
+  }
+  
+  const obj1 = {
+    ...obj,
+    country: 'MX'
+  }
+  
+  console.log(obj1);
+  
+  
+  const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+      (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    });
+  };
+  
+  helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizo'))
+  
+  
+    const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+    const match = regexData.exec('2018-04-20');
+    const year = match[1]
+    const month = match[2]
+    const day = match[3]
+  
+    console.log(year, month, day);
+
+    function newFunction(name, age, country) {
+        var name = name || 'oscar';
+        var age = age || 32;
+        var country = country || 'MX';
+        console.log(name, age, country);
+      }
+      
+      // es6
+      function newFunction2(name = 'oscar', age = 32, country = "MX") {
+        console.log(name, age, country);
+      };
+      
+      newFunction2();
+      newFunction2('Ricardo', '23', 'CO');
+      
+      let hello = "Hello";
+      let world = "World";
+      let epicPhrase = hello + ' ' + world;
+      console.log(epicPhrase);
+      let epicPhrase2 = `${hello} ${world}`;
+      console.log(epicPhrase2);
+      
+      let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
+        + "otra frase epica que necesitamos."
+      
+      // es6
+      let lorem2 = `otra frase epica que necesitamos
+      ahora es otra frase epica
+      `;
+      
+      console.log(lorem);
+      console.log(lorem2);
+      
+      let person = {
+        'name': 'oscar',
+        'age': 32,
+        'country': 'MX'
+      }
+      
+      console.log(person.name, person.age);
+      
+      let { name1 } = person;
+      console.log(name1);
+      
+      let team1 = ['Oscar', 'Julian', 'Ricardo'];
+      let team2 = ['Valeria', 'Yesica', 'Camila'];
+      
+      let education = ['David', ...team1, ...team2];
+      
+      console.log(education);
+      
+      {
+        var globalVar = "Global Var";
+      }
+      
+      {
+        let globalLet = 'Global Let';
+        console.log(globalLet);
+      }
+      
+      console.log(globalVar);
+      
+      const a = 'b';
+      a = 'a';
+      console.log(a);
+      
+      let name = 'oscar';
+      let age = 32;
+      
+      //es5
+      obj = { name: name, age: age };
+      //es6
+      obj2 = { name, age };
+      console.log(obj2);
+      
+      const names = [
+        { name: 'Oscar', age: 32 },
+        { name: 'Yesica', age: 27 }
+      ]
+      
+      let listOfNames = names.map(function (item) {
+        console.log(item.name);
+      })
+      
+      let listOfNames2 = names.map(item => console.log(item.name));
+      
+      const listOfNames3 = (name, age, country) => {
+         
+      }
+      
+      const listOfNames4 = name => {
+        
+      }
+      
+      const square = num => num * num;
+      
+      const helloPromise = () => {
+        return new Promise((resolve, reject) => {
+          if (false) {
+            resolve('Hey!');
+          } else {
+            reject('Ups!!');
+          }
+        });
+      }
+      
+      helloPromise()
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+      
+      class calculator {
+        constructor() {
+          this.valueA = 0;
+          this.valueB = 0;
+        }
+        sum(valueA, valueB) {
+          this.valueA = valueA;
+          this.valueB = valueB;
+          return this.valueA + this.valueB;
+        }
+      }
+      
+      const calc = new calculator();
+      console.log(calc.sum(2, 2));
+      
+      import { hello } from './module';
+      
+      hello();
+      
+      function* helloWorld() {
+        if (true) {
+          yield 'Hello, ';
+        }
+        if (true) {
+          yield 'World';
+        }
+      };
+      
+      const generatorHello = helloWorld();
+      console.log(generatorHello.next().value);
+      console.log(generatorHello.next().value);
+      console.log(generatorHello.next().value);
